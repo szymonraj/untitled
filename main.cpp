cd0152e78a1c7597a41d9ed65d977f4a9170b1b8
student <245907@edu.p.lodz.pl>
2022-10-03 11:42:12 +0200

    Int Policz (int a, int b) { Return a+b; }

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+Int Policz (int a, int b) {
+    Return a+b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std:: cout <<”W pierwszym branchu; 2+3=” << (Policz2,3) <<std::endl;
     return 0;
 }
